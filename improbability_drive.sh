dd0448d8457d76822bb77aad2d4c9ebd654f2065
P3PP3R <YOUR_EMAIL>
2023-01-07 18:19:36 -0600

    fixed instance links

--- improbability_drive.sh
+++ improbability_drive.sh
@@ -1,45 +1,53 @@
-rann=$(shuf -i 0-99 -n 1)
-
-# you can rip these by logging in vrchat and poaching them from the headers 
+# your secret keys
 apiKey=""
 auth=""
+WEBHOOK=""
 
-# todo: this could be made a little kinder to the vrchat api
-# on closer expection "shuffle" isn't very random, so we introduce our own randomness 
-curl 'https://vrchat.com/api/1/worlds?maxUnityVersion=2019.4.31f1&sort=shuffle&tag=system_approved&order=descending&n=100' \
-    -X 'GET' \
-    -H "Cookie: apiKey=${apiKey}; auth=${auth}" \
-    -H 'Accept: application/json; charset=utf-8' \
-    -H 'Host: vrchat.com' \
-    -H 'User-Agent: Improbability Drive Mk.1' \
-    -H 'Connection: keep-alive' \
-    --output - \
-| jq ".[${rann}]" > world.json 
+function get_vrc {
+    curl $1 \
+        -X 'GET' \
+        -H "Cookie: apiKey=$2; auth=$3" \
+        -H 'Accept: application/json; charset=utf-8' \
+        -H 'User-Agent: Improbability Drive Mk.1.1' \
+        -H 'Connection: keep-alive'
+}
 
-# bot data
-USERNAME="Improbability Drive Mk.1"
+function post_vrc {
+    curl \
+        -X POST \
+        -g \
+        -d $1 \
+        -H 'Content-Type: application/json' \
+        --url $2
+}
+
+rann=$(shuf -i 0-99 -n 1)
+
+worlds_api="https://vrchat.com/api/1/worlds?maxUnityVersion=2019.4.31f1&sort=shuffle&tag=system_approved&order=descending&n=100"
+
+get_vrc $worlds_api $apiKey $auth | jq ".[${rann}]" > world.json 
+
+world_id=$(cat world.json | jq -r .id)
+instance_id="12345"
+user_id="usr_b3073d77-20b9-4a59-b902-c342540b3dd3"
+
+instance_URL="https://vrchat.com/api/1/instances/${world_id}:${instance_id}~hidden(${user_id})~region(us)~nonce(9bad2427-f458-40d1-85b2-6b4a5b260f55)/shortName?permanentify=true"
+
+get_vrc $instance_URL $apiKey $auth
+
+USERNAME="Improbability Drive Mk.1.1"
 AVATAR_URL="https://static.wikia.nocookie.net/hitchhikers/images/7/74/Infinitedrive.jpg"
-
-# world data 
 TITLE=$(cat world.json | jq -r .name)
-# todo: this can be improved to generate instances instead
-URL="https://vrchat.com/home/launch?worldId="$(cat world.json | jq -r .id)
+launch_url="https://vrchat.com/home/launch?worldId=${world_id}&instanceId=${instance_id}~hidden(usr_b3073d77-20b9-4a59-b902-c342540b3dd3)~region(us)~nonce(9bad2427-f458-40d1-85b2-6b4a5b260f55)"
 THUMBNAIL_URL=$(cat world.json | jq -r .thumbnailImageUrl)
 AUTHOR_NAME=$(cat world.json | jq -r .authorName)
 
 # discord embed 
-PAYLOAD="{\"username\":\"${USERNAME}\",\"avatar_url\":\"${AVATAR_URL}\",\"embeds\":[{\"title\":\"${TITLE}\",\"url\":\"${URL}\",\"thumbnail\":{\"url\":\"${THUMBNAIL_URL}\"},\"author\":{\"name\":\"${AUTHOR_NAME}\"}}]}"
+PAYLOAD="{\"username\":\"${USERNAME}\",\"avatar_url\":\"${AVATAR_URL}\",\"embeds\":[{\"title\":\"${TITLE}\",\"url\":\"${launch_url}\",\"thumbnail\":{\"url\":\"${THUMBNAIL_URL}\"},\"author\":{\"name\":\"${AUTHOR_NAME}\"}}]}"
 
-# curl throws a fit about the format of the json when provied inline. Passing as a file is fine
+# curl throws a fit about the format of the json when provided inline. Passing as a file is fine
 echo $PAYLOAD | jq '.' > world_payload.json
 
-# generate these from discord chanel options
-WEBHOOK=""
+payload="@world_payload.json"
 
-curl \
-    -X POST \
-    -g \
-    -d @world_payload.json \
-    -H 'Content-Type: application/json' \
-    --url $WEBHOOK
-
+post_vrc $payload $WEBHOOK
